56d960a965de573ce7476778cb89875f7e80a8fa
leejason28 <jason928lee@gmail.com>
2023-05-18 21:09:43 -0700

    finished implementation w script

--- lib/main.rb
+++ lib/main.rb
@@ -14,7 +14,7 @@ p "post order:"
 p t.postorder
 p "in order:"
 p t.inorder
-insertion_arr = Array.new(5) { rand(101..200) }
+insertion_arr = Array.new(10) { rand(101..200) }
 for i in 0...insertion_arr.uniq.length
   t.insert(insertion_arr.uniq[i])
 end
